7c0c5a99c625e81eb96f96d474a994a7bec75f32
ik7719 <imlogic@naver.com>
2022-05-14 00:49:17 +0900

    가운데 글자 가져오기

---
+++ src/main/java/Q3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args){
+        String answer = "";
+
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+
+        int b = a / 2;
+
+        // [0]과 [변수.length] 제거
+        // 나머지 배열의 값을 출력
+
+        if(b % 2 == 0){
+            int[] intEven = new int[b];
+                System.out.println(answer);
+            }
+        } else {
+            for(int i = 1; i < a-2; i++ ){
+                System.out.println(answer);
+            }
+        }
+    }
+}
