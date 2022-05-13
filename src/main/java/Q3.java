f3fb2a826443185eb7e07e9508e12acc7c040660
ik7719 <imlogic@naver.com>
2022-05-14 01:07:07 +0900

    자기 전 테스트 - 오류

--- src/main/java/Q3.java
+++ src/main/java/Q3.java
@@ -14,24 +14,25 @@ public class Q3 {
         // [0]과 [변수.length] 제거
         // 나머지 배열의 값을 출력
 
-
-
-        if(b % 2 == 0){
+        if(b % 2 == 0) {
             int[] intEven = new int[b];
-            intEven[0].remove;
-            intEven[length].remove;
+            intEven.remove(0);
+            intEven.remove(intEven[length]);
             // intEven이라는 변수가 있는데 양 끝 배열 index값은 제거됨.
             // 나머지 배열의 값들 출력
             // intEven은 정수타입 / answer는 문자타입
 
-                System.out.println(Arrays.toString(intEven));
-            }
-        } else {
-        int[] intOdd = new int[b];
-        intOdd[0].remove;
-        intOdd[length].remove;
+            System.out.println(Arrays.toString(intEven));
+        }
 
-        System.out.println(Arrays.toString(intEven));
+//        else {
+//        int[] intOdd = new int[b];
+//        intOdd.remove(0);
+//        intOdd.remove(intOdd[length]);
+//
+//        System.out.println(Arrays.toString(intEven));
+//        }
+        
 
     }
 }
