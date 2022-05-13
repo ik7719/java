9bd6ba630d84f6736cd868195a5c17713f65352a
ik7719 <imlogic@naver.com>
2022-05-14 00:59:46 +0900

    테스트

--- src/main/java/Q3.java
+++ src/main/java/Q3.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args){
-        String answer = "";
+//        String answer = "";
 
         Scanner scan = new Scanner(System.in);
 
@@ -13,14 +14,24 @@ public class Q3 {
         // [0]과 [변수.length] 제거
         // 나머지 배열의 값을 출력
 
+
+
         if(b % 2 == 0){
             int[] intEven = new int[b];
-                System.out.println(answer);
+            intEven[0].remove;
+            intEven[length].remove;
+            // intEven이라는 변수가 있는데 양 끝 배열 index값은 제거됨.
+            // 나머지 배열의 값들 출력
+            // intEven은 정수타입 / answer는 문자타입
+
+                System.out.println(Arrays.toString(intEven));
             }
         } else {
-            for(int i = 1; i < a-2; i++ ){
-                System.out.println(answer);
-            }
-        }
+        int[] intOdd = new int[b];
+        intOdd[0].remove;
+        intOdd[length].remove;
+
+        System.out.println(Arrays.toString(intEven));
+
     }
 }
