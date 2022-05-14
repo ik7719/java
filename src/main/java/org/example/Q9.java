f3f95cdd18046f9215ea08c0b1b6000d95f0b57f
ik7719 <imlogic@naver.com>
2022-05-14 18:00:41 +0900

    숫자 7로 자르면 안됨. 수정 필요

--- src/main/java/org/example/Q9.java
+++ src/main/java/org/example/Q9.java
@@ -12,20 +12,25 @@ public class Q9 {
 //        String answer = "";
 
         String rearNum = phone_number;
-        String frontNum = phone_number;
+//        String frontNum = phone_number;
 
         // 앞에 7줄만 별로 표기
-        String frontResult = frontNum.substring(0,7); // 앞에서 7 인덱스를 남기고 나머지 제거
+//        String frontResult = frontNum.substring(0,7); // 앞에서 7 인덱스를 남기고 나머지 제거
 
         // 별모양으로 표기
+        String star = "";
 
+        for(int i = 0; i < 7; i++){
+            star += "*";
+        }
+        System.out.print(star);
 
         //뒤에 4 단어는 숫자로 표기
         String rearResult = rearNum.substring(rearNum.length()-4, rearNum.length());
         // 뒤에 4 단어만 잘라서 보여줌
 //        String rearNum = rearNum.substring(4, rearNum.length()); // 앞에 4단어를 자르고 보여줌.
 
-        System.out.println(frontResult);
+//        System.out.println(frontResult);
         System.out.println(rearResult);
 
 //        answer = result;
