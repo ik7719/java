20bd7d28c9827284762d60b28075fffc2e5429fd
ik7719 <imlogic@naver.com>
2022-05-14 17:53:17 +0900

    앞자리 7자리 숫자 뒤자리 4자리 숫자

--- src/main/java/org/example/Q9.java
+++ src/main/java/org/example/Q9.java
@@ -10,10 +10,23 @@ public class Q9 {
         String phone_number = scan.next();
 
 //        String answer = "";
-        String number = phone_number;
 
-        String result = number.substring(number.length()-4, number.length());
-        System.out.println(result);
+        String rearNum = phone_number;
+        String frontNum = phone_number;
+
+        // 앞에 7줄만 별로 표기
+        String frontResult = frontNum.substring(0,7); // 앞에서 7 인덱스를 남기고 나머지 제거
+
+        // 별모양으로 표기
+
+
+        //뒤에 4 단어는 숫자로 표기
+        String rearResult = rearNum.substring(rearNum.length()-4, rearNum.length());
+        // 뒤에 4 단어만 잘라서 보여줌
+//        String rearNum = rearNum.substring(4, rearNum.length()); // 앞에 4단어를 자르고 보여줌.
+
+        System.out.println(frontResult);
+        System.out.println(rearResult);
 
 //        answer = result;
     }
