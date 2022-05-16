b692cccff147b7ac91651ccf4f583af394525604
ik7719 <imlogic@naver.com>
2022-05-17 00:56:33 +0900

    Q5 문자열을 정수로 바꾸기

---
+++ src/main/java/new_prac/Q5_prac.java
@@ -0,0 +1,12 @@
+package new_prac;
+
+public class Q5_prac {
+
+    public static void main(String[] args){
+        String s = "-2345";
+
+        int answer = Integer.valueOf(s);
+
+        System.out.println(answer);
+    }
+}
