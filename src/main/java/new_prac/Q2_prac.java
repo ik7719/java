e08730b5fa979a3f50912ca40a49aded01112af5
ik7719 <imlogic@naver.com>
2022-05-16 23:59:11 +0900

    Q2 가운데 글자 가져오기

---
+++ src/main/java/new_prac/Q2_prac.java
@@ -0,0 +1,13 @@
+package new_prac;
+
+public class Q2_prac {
+    public static void main(String[] args){
+        int num = 7;
+
+        if(num % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
