061874daa454a27d1a93e9003f542f0252a1f083
ik7719 <imlogic@naver.com>
2022-05-16 14:00:32 +0900

    Q14 나누어 떨어지는 숫자 배열

---
+++ src/main/java/org/example/Q14.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Q14 {
+    public static void main(String[] args){
+        int arr[] = new int[] {5, 9, 7, 10};
+        int divisor = 5;
+        int answer = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] / divisor == 0){
+                // 그 값을 반환해라
+                // 아니면 나눈 값을 저장하고 그 값을 다시 divisor로 곱한 후 배열로 저장
+            }
+        }
+    }
+}
