5ba1f1f5b040c29595931d1f29f355818cdbf5aa
ik7719 <imlogic@naver.com>
2022-05-16 15:01:17 +0900

    Q20 완주하지 못한 선수 완료

--- src/main/java/org/example/Q20.java
+++ src/main/java/org/example/Q20.java
@@ -1,22 +1,22 @@
 package org.example;
 
-import javax.annotation.processing.Completion;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Q20 {
     public static void main(String[] args){
-        String answer = "";
 
-        String[] participant = new String[] {"leo", "kiki", "eden"};
-        String[] completion = new String[] {"leo", "kiki"};
+        List<String> participant = new ArrayList<String>();
+        participant.add("leo");
+        participant.add("kiki");
+        participant.add("eden");
 
-        for(int i = 0; i < participant.length; i++) {
-            if(participant != completion){
-                participant.removeAll(completion);
-                System.out.println("차집합 :" + Participant.toString() );
-            }
-            else {
-                System.out.println("교집합이 없습니다.");
-            }
-        }
+        List<String> completion = new ArrayList<String>();
+        completion.add("leo");
+        completion.add("kiki");
+
+        participant.removeAll(completion);
+
+        System.out.println(participant);
     }
 }
