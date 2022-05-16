cc3ea9a5850aed084669d11f584799966471c1f8
ik7719 <imlogic@naver.com>
2022-05-16 23:52:36 +0900

    Q1 직사각형 별찍기

---
+++ src/main/java/org/example/Q1_prac.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Q1_prac {
+    public static void main(String[] args){
+        int n = 5;
+        int m = 3;
+
+        int i;
+        int j;
+
+        for(i = 0; i < m; i ++){ // 별 5개를 3줄로
+            for(j = 0; j < n; j++){ // 1열에 행 5개
+                System.out.print("*");
+            }
+            System.out.println(); // 개행만 하기 위함.
+        }
+    }
+}
