466a6b1084817b4809992d55dd5770b5eeedc17c
ik7719 <imlogic@naver.com>
2022-05-16 10:46:52 +0900

    Q16 초기화

--- src/main/java/org/example/Q16.java
+++ src/main/java/org/example/Q16.java
@@ -10,24 +10,5 @@ public class Q16 {
 
         String str = scan.nextLine();
 
-        System.out.println(countChar(str, 'p'));
-        System.out.println(countChar(str, 'y'));
-
-
-    }
-
-    public static int countChar(String str, char ch) {
-        int count = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) { // charAt : 문자열에서 문자를 뽑는 함수
-                count++;
-            }
-        }
-
-
-
-        return count;
     }
 }
-
