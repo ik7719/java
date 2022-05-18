3ced727da947688d31960c440f7e5d58e2259811
ik7719 <imlogic@naver.com>
2022-05-18 14:09:03 +0900

    버스 최초 상태 추가

--- src/main/java/workingSpace/Bus.java
+++ src/main/java/workingSpace/Bus.java
@@ -6,6 +6,8 @@ public class Bus {
     int Num; // 번호
     int oil; // 주유량
     int passenger; // 탑승객
+    String run; // 운행중
+    String stopRun; // 미운행
 
     public Bus(int num){
         this.Num = num;
@@ -24,8 +26,14 @@ public class Bus {
         return passenger;
     }
 
+    public String drive(){
+        run = "운행중입니다.";
+        return run;
+    }
+
     public void showInfo(){
-        System.out.println("Bus " + this.getNum() + " got " + this.getPassenger() + " passengers");
+        System.out.println("버스 " + this.getNum() + "의 탑승객은 " + this.getPassenger() + "명입니다.");
+        System.out.println("버스 " + this.getNum() + "은 현재 " + this.drive());
     }
 
 }
--- src/main/java/workingSpace/Test.java
+++ src/main/java/workingSpace/Test.java
@@ -4,9 +4,9 @@ import java.util.UUID;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println("객체 생성시 번호는 고유값으로 생성되어야 합니다.");
-        System.out.println("uuid - 고유 값을 표현할 수 있는 객체입니다 (32개 문자와 하이픈 포함한 총 36개값)");
-        System.out.println("start, center, end 부분에 생성하려는 데이터 값을 삽입하시면 됩니다");
+//        System.out.println("객체 생성시 번호는 고유값으로 생성되어야 합니다.");
+//        System.out.println("uuid - 고유 값을 표현할 수 있는 객체입니다 (32개 문자와 하이픈 포함한 총 36개값)");
+//        System.out.println("start, center, end 부분에 생성하려는 데이터 값을 삽입하시면 됩니다");
 
         String start = "20210203075533";
         String center = "서울 70 사 5705"; // 특정 고유값 지정
