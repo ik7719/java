dfe907034f3470e1972e259a02092a1ceea48478
ik7719 <imlogic@naver.com>
2022-05-19 09:30:21 +0900

    택시 세팅

---
+++ src/main/java/Taxi.java
@@ -0,0 +1,12 @@
+public class Taxi {
+    private int taxiNumber; //타입 뭘로할까?
+    private int fuel;
+    private double currentSpeed;
+    private String goal;
+    private double distance;
+    private double goalDistance;
+    private int fee;
+    private int distanceFee;
+
+
+}
