39ba1bd4bf9a3aff2d47fbb898b2300d731cce11
ik7719 <imlogic@naver.com>
2022-05-19 10:04:24 +0900

    요구사항 추가 - 조건이 맞을 때

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Taxi taxi = new Taxi(10, 20, 30, 30, 1500, 1000);
+        Taxi taxi = new Taxi(9, 20, 30, 30, 1500, 1000);
+
+        taxi.drive();
+        taxi.ride();
+        taxi.speedChange();
+        taxi.addFee();
+        taxi.payFee();
+
     }
 }
--- src/main/java/Taxi.java
+++ src/main/java/Taxi.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Taxi {
     private int taxiNumber; //타입 뭘로할까?
     private int fuel;
@@ -7,6 +8,7 @@ public class Taxi {
     private double goalDistance;
     private int fee;
     private int distanceFee;
+    private condition taxiCondition;
 
     public Taxi(int fuel, double currentSpeed, double distance, double goalDistance, int fee, int distanceFee){
         this.fuel = fuel;
@@ -16,7 +18,43 @@ public class Taxi {
         this.goalDistance = goalDistance;
         this.fee = fee;
         this.distanceFee = distanceFee;
+        taxiCondition = condition.일반;
     }
 
+    public void drive(){
+        if(fuel < 10){
+            System.out.println("주유량이 부족해 운행이 불가능합니다.");
+            return;
+        }
+        System.out.println("탑승이 가능합니다.");
+    }
 
+    public void ride(){
+        if(taxiCondition == condition.일반 || fuel < 10){
+            System.out.println("탑승 불가");
+            return;
+        }
+        taxiCondition = condition.운행중;
+        System.out.println("운행을 시작합니다.");
+    }
+
+    public void speedChange(){
+        System.out.println("현재 속도는 " + currentSpeed + "km/h 입니다." + "추가하고 싶은 속도를 입력해주세요.");
+
+        Scanner scan = new Scanner(System.in);
+        double speedPlus = scan.nextDouble();
+        currentSpeed += speedPlus;
+
+        System.out.println("현재 속도는 " + currentSpeed + "km/h 입니다.");
+    }
+
+    public void addFee(){
+        if(distance < goalDistance){
+            fee += distanceFee;
+        }
+    }
+
+    public void payFee(){
+        System.out.println("최종 요금은 " + fee + "원 입니다.");
+    }
 }
