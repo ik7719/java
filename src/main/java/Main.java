55038ef19bb100a4e6335437662ef7df74ecf99f
ik7719 <imlogic@naver.com>
2022-05-19 09:37:25 +0900

    정보 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Taxi taxi = new Taxi();
+        Taxi taxi = new Taxi(10, 20, 30, 30, 1500, 1000);
     }
 }
--- src/main/java/Taxi.java
+++ src/main/java/Taxi.java
@@ -8,5 +8,15 @@ public class Taxi {
     private int fee;
     private int distanceFee;
 
+    public Taxi(int fuel, double currentSpeed, double distance, double goalDistance, int fee, int distanceFee){
+        this.fuel = fuel;
+        this.currentSpeed = currentSpeed;
+        this.goal = "영월";
+        this.distance = distance;
+        this.goalDistance = goalDistance;
+        this.fee = fee;
+        this.distanceFee = distanceFee;
+    }
+
 
 }
